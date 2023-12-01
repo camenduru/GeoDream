9ea75c468159ec463dc5a269df3a6a18fad8e22c
camenduru <54370274+camenduru@users.noreply.github.com>
2023-12-01 23:44:14 +0300

    # training = self.training

--- threestudio/models/renderers/geodream_volume_renderer.py
+++ threestudio/models/renderers/geodream_volume_renderer.py
@@ -127,7 +127,7 @@ class GeodreamVolumeRenderer(VolumeRenderer):
                     stratified=self.randomized,
                     cone_angle=0.0,
                     early_stop_eps=0,
-                    training = self.training,
+                    # training = self.training,
                 )
         else:
             with torch.no_grad():
@@ -139,7 +139,7 @@ class GeodreamVolumeRenderer(VolumeRenderer):
                     alpha_thre=0.01 if self.cfg.prune_alpha_threshold else 0.0,
                     stratified=self.randomized,
                     cone_angle=0.0,
-                    training = self.training,
+                    # training = self.training,
                 )
                 
         ray_indices, t_starts_, t_ends_ = validate_empty_rays(
